074f488b4497a9a5d71ed613ec4f544140b58f69
Tetiana Lapshun <lapshun.tatiana@gmail.com>
2025-06-23 16:12:43 +0300

    create

---
+++ Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lapshun Tetiana on 23.06.2025.
+//
+
+#include "Card.h"
---
+++ Card.h
@@ -0,0 +1,14 @@
+//
+// Created by Lapshun Tetiana on 23.06.2025.
+//
+
+#ifndef P44_T20_OOP_CARD_H
+#define P44_T20_OOP_CARD_H
+
+
+class Card {
+
+};
+
+
+#endif //P44_T20_OOP_CARD_H
